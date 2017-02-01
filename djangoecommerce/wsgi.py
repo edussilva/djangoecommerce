53eaadedce2444970da43468b742b5888dbf427b
edussilva <edussilva@live.com>
2017-01-31 22:45:33 -0200

    teste

--- djangoecommerce/wsgi.py
+++ djangoecommerce/wsgi.py
@@ -18,5 +18,9 @@ application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
+
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
