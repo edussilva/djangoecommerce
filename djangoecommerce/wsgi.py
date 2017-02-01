9d99cbb6b4418091abad25866815428f63f92195
edussilva <edussilva@live.com>
2017-01-31 22:47:01 -0200

    teste

--- djangoecommerce/wsgi.py
+++ djangoecommerce/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,13 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
 
 application = get_wsgi_application()
+
 """
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
-
-
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+"""
